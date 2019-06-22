638070f4015ee1a3976df67f3e6b19bc750d6e38
GrandArth <v2629994284@163.com>
2019-06-22 17:00:47 +0800

    第三章的第一题

--- pptalg/chapter3.cpp
+++ pptalg/chapter3.cpp
@@ -3,9 +3,31 @@
 using namespace std;
 struct node{
 	int data;
-	int ind;
+	int ind; //ind ==-1 尾节点 ind==1 头节点 int== <else>普通节点
 	struct node* nextnode;
 
 }
 typedef struct node * list;
+
+void qmpty(list li) //这里的li代之指向tail的节点
+{
+li->next=li;
+}
+
+list qin(list li, int num)
+{
+	list newend = new struct ndoe;
+	newend->data=num;
+	newend->ind=-1;
+	newend->nextnode=NULL;
+	li->ind=0;
+	li->nextnode=newend;
+	return newend;
+}
+
+void qout(list li)
+{
+	li->nextnode=li->nextnode->nextnode;
+}
+
 
