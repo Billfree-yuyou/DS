94e2f131f011ebfe87ec019ab71225a6a4efbd48
Cloud User <centos@ip-172-31-25-71.us-east-2.compute.internal>
2019-06-22 07:36:42 +0000

    refined

--- pptalg/pptlist.cpp
+++ pptalg/pptlist.cpp
@@ -1,25 +1,31 @@
-$
+
 #include<vector>
 #include<iostream>
 using namespace std;
 void insertlist(int *a,int num)
 {
 	int i=0;
-	int b[]=a;
+	int temp;
 	int rec=0;
 	while(num>=a[i])
 	{
 		i++;
 	}
 	rec=i;
-	while(b[i])
+	while(a[i])
 	{
-		a[i+1]=b[i];
 		i++;
+	}
+	temp=i-1;
+	while(temp>=rec)
+	{
+		a[temp+1]=temp;
+		temp--;
 	}
 	a[rec]=num;
 
 }
+int main(){return 0;}
 
 //The analysis of time:
 //array cpoy fot b cost n
